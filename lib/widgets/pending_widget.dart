b4a1f18ffa107047deba348ccc4ca6a889ee5385
sachithnim <nimendras07@gmail.com>
2024-08-14 20:35:01 +0530

    Complete Edit section

--- lib/widgets/pending_widget.dart
+++ lib/widgets/pending_widget.dart
@@ -63,7 +63,7 @@ class _PendingWidgetState extends State<PendingWidget> {
                       icon: Icons.edit,
                       label: "Edit",	
                       onPressed: (context){
-                      _databaseServices.updateTodoStatus(todo.id, true);
+                      _showTaskDialog(context, todo: todo);
                     }),
                     SlidableAction(
                       backgroundColor: Colors.red,
@@ -96,6 +96,73 @@ class _PendingWidgetState extends State<PendingWidget> {
           return Center(child: CircularProgressIndicator(color: Colors.white));
         }
       },
+    );
+  }
+  void _showTaskDialog(BuildContext context,{Todo? todo}) {
+    final TextEditingController _titleController = TextEditingController(text: todo?.title);
+    final TextEditingController _descriptionController = TextEditingController(text: todo?.description);
+    final DatabaseServices _databaseService = DatabaseServices();
+
+
+    showDialog (context: context, builder: (context) {
+      return AlertDialog(
+        backgroundColor: Colors.white,
+        title: Text(
+          todo == null? "Add Task" : "Edit Task",
+          style: TextStyle(
+          fontWeight: FontWeight.bold,
+          ),
+        ),
+        content: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  labelText: "Title",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: _descriptionController,
+                decoration: InputDecoration(
+                  labelText: "Description",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            child: Text("Cancel"),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.indigo,
+              foregroundColor: Colors.white,
+            ),
+          onPressed: () async {
+            if(todo == null){
+              await _databaseService.addToTask(_titleController.text, _descriptionController.text);
+            }
+            else{
+              await _databaseService.updateTodo(todo.id, _titleController.text, _descriptionController.text);
+            }
+          
+            Navigator.pop(context);
+          }, child: Text(todo == null ? "Add" : "Update"),
+          ),
+        ]
+      );
+    },
     );
   }
 }
